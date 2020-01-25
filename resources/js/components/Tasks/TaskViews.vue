b8a80027a71287aa12570a7a6bcd95f3ab0c8760
John <sbcman74@gmail.com>
2020-01-24 21:10:30 -0600

    fixed button alignment on tasks views

--- resources/js/components/Tasks/TaskViews.vue
+++ resources/js/components/Tasks/TaskViews.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <div class="flex items-center justify-between mb-4">
-      <div class="mr-4">
+      <div class="flex items-center mr-4">
         <button
           class="w-5 h-5 mr-4 text-gray-500 fill-current"
           @click="setView('tile')"
