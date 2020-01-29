7ed996565c10fc7cf38d478f00c6620880f5f894
John <sbcman74@gmail.com>
2020-01-29 16:10:37 -0600

    added covertfrommilliseconds function

--- resources/js/components/WorkEntry/WorkEntryTable.vue
+++ resources/js/components/WorkEntry/WorkEntryTable.vue
@@ -273,9 +273,16 @@ export default {
       );
     },
     convertFromMilliseconds(time) {
-      //   return moment
-      //     .utc(moment.duration(time, "milliseconds").asMilliseconds())
-      //     .format("HH:mm:ss");
+      let milliseconds = parseInt((time % 1000) / 100);
+      let seconds = Math.floor((time / 1000) % 60);
+      let minutes = Math.floor((time / (1000 * 60)) % 60);
+      let hours = Math.floor((time / (1000 * 60 * 60)) % 24);
+
+      hours = hours < 10 ? "0" + hours : hours;
+      minutes = minutes < 10 ? "0" + minutes : minutes;
+      seconds = seconds < 10 ? "0" + seconds : seconds;
+
+      return hours + ":" + minutes + ":" + seconds;
     }
   }
 };
