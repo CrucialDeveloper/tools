11709b5507b6335617c22526d2eca596c6058ea2
johnlowery <sbcman74@gmail.com>
2021-02-14 14:53:46 -0600

    campaign test

--- routes/web.php
+++ routes/web.php
@@ -36,8 +36,3 @@ Route::post('html-cleaner', [HtmlCleanerController::class, 'store']);
 
 Route::get('/aem-groups', [AemGroupController::class, 'index']);
 Route::post('/aem-groups', [AemGroupController::class, 'create']);
-
-
-Route::post('/campaign', function (Request $request) {
-    log($request);
-});
